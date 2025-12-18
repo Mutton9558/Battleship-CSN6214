25451daccb10871519ab754b06d3087df319f6b1
Mutton9558 <htp8090@gmail.com>
2025-12-18 13:21:43 +0800

    Initial files of this project

---
+++ Server/client_session.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <pthread.h>
+
+int main(){
+    printf("Forked processes to interface between Client's game UI and Server");
+    return 0;
+}
---
+++ Server/server.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(){
+    printf("Big boy server thing uwu");
+    return 0;
+}
